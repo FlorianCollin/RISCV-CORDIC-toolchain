48964fa25806ccef6ec31d6214ef2950b4e755e8
Florian Collin <125576600+FlorianCollin@users.noreply.github.com>
2024-03-23 12:53:59 +0100

    Add files via upload

--- libcordic/riscveirb_benchmark_1.s
+++ libcordic/riscveirb_benchmark_1.s
@@ -1,5 +1,18 @@
-li t0, 2
-li t1, 30
-add t2, t0, t1
-sw t2, 0x8(x0)
-ret
+.section .text
+.globl _start
+
+_start:
+    # Initialisation
+    li t0, 6            # Charger la valeur 6 dans le registre t0
+    li t1, 10           # Charger la valeur 10 dans le registre t1
+
+    # Addition
+    add t2, t0, t1      # Additionner t0 et t1, stocker le résultat dans t2
+
+    # Stockage du résultat dans la mémoire
+    sw t2, 0(x0)        # Stocker la valeur de t2 à l'adresse 0
+
+loop:
+    j loop              # Boucler indéfiniment sur cette instruction
+
+# Fin du programme
